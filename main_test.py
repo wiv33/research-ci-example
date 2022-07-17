bd1a2142e9c59f9222cd8bb4f982bf2344443727
PSK <wiv821253@gmail.com>
2022-07-17 18:24:42 +0900

    init commit

--- main_test.py
+++ main_test.py
@@ -6,7 +6,7 @@ import main
 class MainTest(unittest.TestCase):
     def test_helloworld(self):
         result = main.helloworld("Test")
-        self.assertEquals("hello world! ", result)
+        self.assertEquals("hello world! Test", result)
 
 
 if __name__ == "__main__":  # pragma: no cover
